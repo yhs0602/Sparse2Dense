d6ddd02f86f5a1b2a9592a39975f18613d09395d
yhs0602 <jourhyang123@gmail.com>
2024-04-23 17:23:25 +0900

    :sparkles: Add cross goals

---
+++ cross/cross_env.py
@@ -0,0 +1,65 @@
+import os.path
+from typing import Tuple
+
+from craftground import craftground
+from craftground.craftground import CraftGroundEnvironment
+from craftground.craftground.screen_encoding_modes import ScreenEncodingMode
+
+from utils.check_vglrun import check_vglrun
+
+# ./nbt/cross.nbt
+current_folder_path = os.path.dirname(os.path.abspath(__file__))
+nbts_path = os.path.join(current_folder_path, "nbt")
+map_path = os.path.join(nbts_path, "cross.nbt")
+
+INITIAL_POSITION = [2, 2, 6, -90, 0]
+
+CROSS_GOALS = [
+    (7, 2, 1),  # 왼쪽
+    (12, 2, 6),  # 앞쪽
+    (7, 2, 11),  # 오른쪽
+]
+
+
+def make_cross_env(
+    port: int, size_x: int, size_y: int
+) -> Tuple[CraftGroundEnvironment, list[str]]:
+    return (
+        craftground.make(
+            port=port,
+            initialInventoryCommands=[],
+            verbose=False,
+            initialPosition=[5, 5, 5],  # nullable
+            initialMobsCommands=[],
+            imageSizeX=size_x,
+            imageSizeY=size_y,
+            visibleSizeX=size_x,
+            visibleSizeY=size_y,
+            seed=12345,  # nullable
+            allowMobSpawn=False,
+            alwaysDay=True,
+            alwaysNight=False,
+            initialWeather="clear",  # nullable
+            isHardCore=False,
+            isWorldFlat=True,  # superflat world
+            obs_keys=[],  # No sound subtitles
+            miscStatKeys=[],  # No stats
+            initialExtraCommands=[
+                "time set noon",
+                "place template minecraft:cross 0 0 0",
+                "tp @p 2 2 6 -90 0",
+                "effect give @p minecraft:speed infinite 1 true",  # speed effect, particle hidden
+            ],  # x y z yaw pitch
+            isHudHidden=True,
+            render_action=False,
+            render_distance=5,
+            simulation_distance=5,
+            structure_paths=[
+                map_path,
+            ],
+            no_pov_effect=True,
+            screen_encoding_mode=ScreenEncodingMode.RAW,
+            use_vglrun=check_vglrun(),
+        ),
+        [],
+    )
