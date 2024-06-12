7e73831022421fc9e9bd178b082815220062b921
yhs0602 <jourhyang123@gmail.com>
2024-06-12 17:21:27 +0900

    :sparkles: Stats

---
+++ post_processing/gen_csv_from_json.py
@@ -0,0 +1,95 @@
+import pandas as pd
+
+data = {
+    "v30-crossw2-sparse-2": {
+        "mean_length": 1471.0238037109375,
+        "mean_reward": 0.8528976099831718,
+        "mean_eval_episodes": 318.85714285714283,
+        "mean_eval_left_success_count": 101.57142857142857,
+        "mean_eval_middle_success_count": 102.57142857142857,
+        "mean_eval_right_success_count": 101.28571428571429,
+        "mean_eval_total_success_count": 305.42857142857144,
+    },
+    "v30-crossw2-dense-0": {
+        "mean_length": 898.04443359375,
+        "mean_reward": 0.9136844674746195,
+        "mean_eval_episodes": 330.6666666666667,
+        "mean_eval_left_success_count": 106.66666666666667,
+        "mean_eval_middle_success_count": 106.33333333333333,
+        "mean_eval_right_success_count": 106.0,
+        "mean_eval_total_success_count": 319.0,
+    },
+    "v30-crossw2-dense-2": {
+        "mean_length": 1221.6778055826824,
+        "mean_reward": 0.8820211191972097,
+        "mean_eval_episodes": 315.1666666666667,
+        "mean_eval_left_success_count": 100.0,
+        "mean_eval_middle_success_count": 101.66666666666667,
+        "mean_eval_right_success_count": 100.0,
+        "mean_eval_total_success_count": 301.6666666666667,
+    },
+    "v30-crossw2-transition-3000000-1": {
+        "mean_length": 1214.7388712565105,
+        "mean_reward": 0.87852610150973,
+        "mean_eval_episodes": 423.6666666666667,
+        "mean_eval_left_success_count": 135.83333333333334,
+        "mean_eval_middle_success_count": 136.66666666666666,
+        "mean_eval_right_success_count": 135.33333333333334,
+        "mean_eval_total_success_count": 407.8333333333333,
+    },
+    "v30-crossw2-transition-3000000-2": {
+        "mean_length": 2131.8943990071616,
+        "mean_reward": 0.7868105471134186,
+        "mean_eval_episodes": 310.0,
+        "mean_eval_left_success_count": 99.5,
+        "mean_eval_middle_success_count": 99.83333333333333,
+        "mean_eval_right_success_count": 99.83333333333333,
+        "mean_eval_total_success_count": 299.1666666666667,
+    },
+    "v30-crossw2-transition-3000000-0": {
+        "mean_length": 1356.7611083984375,
+        "mean_reward": 0.8643238743146261,
+        "mean_eval_episodes": 434.0,
+        "mean_eval_left_success_count": 137.0,
+        "mean_eval_middle_success_count": 139.5,
+        "mean_eval_right_success_count": 136.5,
+        "mean_eval_total_success_count": 413.0,
+    },
+    "v30-crossw2-sparse-0": {
+        "mean_length": 1295.8571602957588,
+        "mean_reward": 0.8704142825944083,
+        "mean_eval_episodes": 363.14285714285717,
+        "mean_eval_left_success_count": 115.85714285714286,
+        "mean_eval_middle_success_count": 116.85714285714286,
+        "mean_eval_right_success_count": 115.28571428571429,
+        "mean_eval_total_success_count": 348.0,
+    },
+    "v30-crossw2-dense-1": {
+        "mean_length": 983.9277648925781,
+        "mean_reward": 0.9055627882480621,
+        "mean_eval_episodes": 392.6666666666667,
+        "mean_eval_left_success_count": 126.33333333333333,
+        "mean_eval_middle_success_count": 126.5,
+        "mean_eval_right_success_count": 126.66666666666667,
+        "mean_eval_total_success_count": 379.5,
+    },
+    "v30-crossw2-sparse-1": {
+        "mean_length": 1884.685756138393,
+        "mean_reward": 0.8067695157868522,
+        "mean_eval_episodes": 318.85714285714283,
+        "mean_eval_left_success_count": 102.71428571428571,
+        "mean_eval_middle_success_count": 102.42857142857143,
+        "mean_eval_right_success_count": 102.57142857142857,
+        "mean_eval_total_success_count": 307.7142857142857,
+    },
+}
+
+if __name__ == "__main__":
+    # 데이터프레임 생성
+    df = pd.DataFrame.from_dict(data, orient="index")
+
+    # CSV 파일로 저장
+    csv_file = "./experiment_results.csv"
+    df.to_csv(csv_file)
+
+    print(f"CSV file created at {csv_file}")
---
+++ post_processing/get_metrics_with_csv.py
@@ -0,0 +1,136 @@
+import pandas as pd
+import wandb
+from wandb.apis.public import HistoryScan
+
+# 이벨류에이션 마지막
+# 마지막 지점으로부터 3개 평균값으로
+#   에피소드랭스,
+#   평균리워드 ,
+#   서세스레이트
+groups = [
+    "v30-crossw2-sparse-2",
+    "v30-crossw2-dense-0",
+    "v30-crossw2-dense-2",
+    "v30-crossw2-transition-3000000-1",
+    "v30-crossw2-transition-3000000-2",
+    "v30-crossw2-transition-3000000-0",
+    "v30-crossw2-sparse-0",
+    "v30-crossw2-dense-1",
+    "v30-crossw2-sparse-1",
+]
+
+
+def main():
+    api = wandb.Api()
+    results = {}
+    for group in groups:
+        runs = api.runs("jourhyang123/craftground-sb3", filters={"group": group})
+        lengths = []
+        rewards = []
+        eval_episodes = []
+        left_success_counts = []
+        middle_success_counts = []
+        right_success_counts = []
+        total_success_counts = []
+
+        for run in runs:
+            # global_history = run.scan_history(keys=["global_step"])
+            # # if len(global_history) == 0:
+            # #     continue
+            # global_step = global_history[-1]["global_step"]
+            global_step = run.summary["global_step"]
+            if global_step < 10000384:
+                print(
+                    f"Skipping run {run.name} from group {group} as it is not finished yet. {global_step}"
+                )
+                continue
+            # del global_history
+            # eval_history: HistoryScan = run.scan_history()
+            # eval_history.to_csv(f"cache/{group}_{run.id}.csv")
+            # keys=[
+            #                     "eval/mean_ep_length",
+            #                     "eval/mean_reward",
+            #                     "eval_episode",
+            #                     "eval_((13, 1, 6), (13, 1, 7))/success_count",
+            #                     "eval_((7, 1, 1), (8, 1, 1))/success_count",
+            #                     "eval_((8, 1, 12), (7, 1, 12))/success_count",
+            #                 ],
+            #                 pandas=False,
+            columns_to_check = [
+                "eval/mean_ep_length",
+                "eval/mean_reward",
+                "eval_episode",
+                "eval_((13, 1, 6), (13, 1, 7))/success_count",
+                "eval_((7, 1, 1), (8, 1, 1))/success_count",
+                "eval_((8, 1, 12), (7, 1, 12))/success_count",
+            ]
+            latest_values = {}
+            # for column in columns_to_check:
+            #     # 마지막 행부터 시작하여 값을 찾습니다.
+            #     for value in eval_history[column][::-1]:
+            #         if pd.notna(value):
+            #             latest_values[column] = value
+            #             break
+            #     else:
+            #         # 값이 전혀 없을 경우
+            #         if column in [
+            #             "eval_((13, 1, 6), (13, 1, 7))/success_count",
+            #             "eval_((7, 1, 1), (8, 1, 1))/success_count",
+            #             "eval_((8, 1, 12), (7, 1, 12))/success_count",
+            #         ]:
+            #             latest_values[column] = 0
+            #         else:
+            #             latest_values[column] = None
+            latest_values = {column: run.summary[column] for column in columns_to_check}
+            if None in latest_values.values():
+                print(
+                    f"Skipping run {run.name} from group {group} as some values are missing. {latest_values}"
+                )
+                continue
+            last_length = latest_values["eval/mean_ep_length"]
+            last_reward = latest_values["eval/mean_reward"]
+            last_eval_episode = latest_values["eval_episode"]
+            left_success_count = latest_values[
+                "eval_((7, 1, 1), (8, 1, 1))/success_count"
+            ]
+            middle_success_count = latest_values[
+                "eval_((13, 1, 6), (13, 1, 7))/success_count"
+            ]
+            right_success_count = latest_values[
+                "eval_((8, 1, 12), (7, 1, 12))/success_count"
+            ]
+            total_success_count = (
+                left_success_count + middle_success_count + right_success_count
+            )
+            lengths.append(last_length)
+            rewards.append(last_reward)
+            eval_episodes.append(last_eval_episode)
+            left_success_counts.append(left_success_count)
+            middle_success_counts.append(middle_success_count)
+            right_success_counts.append(right_success_count)
+            total_success_counts.append(total_success_count)
+
+        if len(lengths) > 0:
+            mean_length = sum(lengths) / len(lengths)
+            mean_reward = sum(rewards) / len(rewards)
+            mean_eval_episodes = sum(eval_episodes) / len(eval_episodes)
+
+            results[group] = {
+                "mean_length": mean_length,
+                "mean_reward": mean_reward,
+                "mean_eval_episodes": mean_eval_episodes,
+                "mean_eval_left_success_count": sum(left_success_counts)
+                / len(left_success_counts),
+                "mean_eval_middle_success_count": sum(middle_success_counts)
+                / len(middle_success_counts),
+                "mean_eval_right_success_count": sum(right_success_counts)
+                / len(right_success_counts),
+                "mean_eval_total_success_count": sum(total_success_counts)
+                / len(total_success_counts),
+            }
+
+    print(results)
+
+
+if __name__ == "__main__":
+    main()
