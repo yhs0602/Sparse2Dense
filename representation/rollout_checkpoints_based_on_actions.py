f039ed093ddcdf7306b4a2e443e324fc40afc200
yhs0602 <jourhyang123@gmail.com>
2024-07-09 14:44:52 +0900

    images

--- representation/rollout_checkpoints_based_on_actions.py
+++ representation/rollout_checkpoints_based_on_actions.py
@@ -279,7 +279,7 @@ def main(port1: int, device_id: int, trajectory_json: str):
     size_y = 64
 
     current_path = os.path.dirname(os.path.abspath(__file__))
-    checkpoint_dir = "./checkpoints"
+    checkpoint_dir = "checkpoints"
     checkpoint_dir = os.path.join(current_path, checkpoint_dir)
 
     trajectory_json = os.path.join(current_path, trajectory_json)
