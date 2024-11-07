2955bd94780eebbf9471b47af2ac910268a62e7f
yhs0602 <jourhyang123@gmail.com>
2024-11-07 10:26:31 +0900

    :fire: remove busybox

--- representation/rollout_checkpoints_based_on_actions.py
+++ representation/rollout_checkpoints_based_on_actions.py
@@ -280,7 +280,7 @@ def main(port1: int, device_id: int, trajectory_json: str, extended: bool):
     size_y = 64
 
     current_path = os.path.dirname(os.path.abspath(__file__))
-    checkpoint_dir = "fixed_seed_model_results"
+    checkpoint_dir = "fixed_seed_model_results_0906"
     checkpoint_dir = os.path.join(current_path, checkpoint_dir)
 
     trajectory_json = os.path.join(current_path, trajectory_json)
